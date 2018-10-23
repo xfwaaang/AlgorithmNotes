6d81b25ae9aa1a5196eddb8e9a19f4a495d82cb6
xfwangmm <xfwangmm@gmail.com>
2018-10-23 20:32:34 +0800

    poj1458

--- DynamicPrograming/lcs.cpp
+++ DynamicPrograming/lcs.cpp
@@ -1,6 +1,6 @@
 /*
 题目描述：
-给定两个序列X和Y，求它们的最长公共子序列(longest common subsequence)
+给定两个序列X和Y，求它们的最长公共子序列的长度(longest common subsequence)
 解题思路：
 设X[m]=(x1,x2,...,xm)和Y[n]=(y1,y2,...,yn)，Z=(z1,z2,...,zk)是X和Y的最长公共子序列，则
 
@@ -14,31 +14,22 @@
 c[i][j] ={ c[i-1][j-1] + 1             i,j>0, xi = yj
            max(c[i-1][j], c[i][j-1])   i,j>0, xi != yj
 
-还需要数组s[m][n]来记录状态，s[i][j]=1,2,3 , 最终用来构造最长公共子序列
 */
 
 #include<iostream>
 #include<string>
+#include<cstdio>
 
 using namespace std;
 
-class LCS
+int LCSLength(string x, string y)
 {
-public:
-	LCS(int nx, int ny, string x, string y);
-	void LCSLength();
-	void CLCS();
+	int m = x.size();
+	int n = y.size();
+	int c[m+1][n+1];
 
-private:
-	void CLCS(int i, int j);
-	int **c, **s, m, n;
-	string lcs;
-};
-
-int LCS::LCSLength()
-{
-	for(int i=1; i<=m; i++)	c[i][0] = 0;
-	for(int i=1; i<=n; i++) c[0][i] = 0;
+	for(int i=0; i<=m; i++)	c[i][0] = 0;
+	for(int i=0; i<=n; i++) c[0][i] = 0;
 
 	for(int i=1; i<=m; i++)
 	{
@@ -47,42 +38,30 @@ int LCS::LCSLength()
 			if(x[i] == y[j])
 			{
 				c[i][j] = c[i-1][j-1] + 1;
-				s[i][j] = 1;
 			}
 			else if(c[i-1][j] >= c[i][j-1])
 			{
 				c[i][j] = c[i-1][j];
-				s[i][j] = 2;	
 			}
 			else
 			{
 				c[i][j] = c[i][j-1];
-				s[i][j] = 3;
 			}
+
 		}
+
 	}
 
 	return c[m][n];
 }
 
-void LCS::CLCS(int i, int j)
-{
-	if(i == 0 || j == 0)	return;
-	if(s[i][j] == 1)
-	{
-		CLCS(i-1, j-1);
-		lcs += x[i];
-	}
-	else if(s[i][j] == 2)
-		CLCS(i-1, j);
-	else
-		CLCS(i, j-1);
-}
-
 int main()
 {
-	string x = "qweugfqwiofrgbiqewbfieqwehydgbiwqbcfdhsiabgcfdbvhaisbvqrhbf";
-	string y = "iwuefbgqawbvhfbuyaqrsdanbjfdsnjuiwuesmlkdjchn";
+	string x, y;
 
-	cout << "最长公共子序列" << endl;
+	while(cin >> x >> y)
+	{
+		cout << LCSLength(x, y) << endl;
+	}
+
 }
