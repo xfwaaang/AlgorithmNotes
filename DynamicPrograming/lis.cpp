a9fe03f9690758339d64e92e89b9e3f5619b1883
xfwaaang <xfwangmm@gmail.com>
2020-06-23 13:02:00 +0800

    最长非降子序列

--- DynamicPrograming/lis.cpp
+++ DynamicPrograming/lis.cpp
@@ -1,8 +1,34 @@
 #include<iostream>
 #include<vector>
+#include"../util.h"
 
 using namespace std;
 
+int solve_2(vector<int>& A)
+{
+	int n = A.size();
+	int max_dp = 1;
+	vector<int> dp(n+1, 1);
+
+	for(int i=2; i<=n; ++i)
+	{
+		int tmp = -1;
+		for(int j=1; j<=i-1; ++j)
+		{
+			if(A[i-1] >= A[j-1])
+			{
+				tmp = max(tmp, dp[j]);
+			}
+		}
+		
+		dp[i] = (tmp == -1 ? 1 : tmp + 1);
+		max_dp = max(max_dp, dp[i]);
+	}
+
+	return max_dp;
+}
+
+//error
 int solve_1(vector<int>& A)
 {
 	int n = A.size();
@@ -25,6 +51,7 @@ int solve_1(vector<int>& A)
 	return help[n];
 }
 
+//erro
 int solve(vector<int>& A)
 {
 	int n = A.size();
@@ -53,7 +80,10 @@ int main()
 	// vector<int> A{5, 3, 4, 6, 2, 7, 8};
 	// vector<int> A{5, 3, 4, 4, 8, 6, 7, 4, 8, 8, 9};
 	// vector<int> A{5};
-	vector<int> A{1, 3, 5, 2, 4};
-	cout << solve(A) << endl;
-	cout << solve_1(A) << endl;
+	vector<int> A = genRandomArray();
+	// vector<int> A{1, 3, 5, 2, 4};
+	printArray(A);
+	cout << solve(A) << endl; //错误
+	cout << solve_1(A) << endl; //错误
+	cout << solve_2(A) << endl; //正确
 }
